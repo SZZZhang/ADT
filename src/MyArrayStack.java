4dc2b8bed636effdfa56cee81bbfd2459d774760
Shirley Zhang <zhangshirleyemail@gmail.com>
2019-11-26 09:49:21 -0500

    finished llist and array implementation of queue and stack

--- src/MyArrayStack.java
+++ src/MyArrayStack.java
@@ -1,2 +1,32 @@
-public class MyArrayStack {
+public class MyArrayStack implements ArrayStack {
+
+    MyArrayList stack = new MyArrayList();
+
+    @Override
+    public void push(Node n) {
+        stack.addNode(n);
+    }
+
+    @Override
+    public Node pop() {
+        Node n = stack.getLastNode();
+        stack.removeNode(stack.size() - 1);
+        return n;
+    }
+
+    @Override
+    public Node peek() {
+        return stack.getLastNode();
+    }
+
+    @Override
+    public int size() {
+        return stack.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (stack.size() == 0) return false;
+        return true;
+    }
 }
